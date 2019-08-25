6589f7b4991a69d5fb37e721f61bd6884cfabe51
Andrew Wu <chicken@chicken-house.net>
2019-08-26 02:22:46 +0800

    add benchmark tools

---
+++ Benchmark/Program.cs
@@ -0,0 +1,32 @@
+﻿using ParallelProcessPractice.Core;
+using System;
+using System.Collections.Generic;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var run in Runners)
+            {
+                Console.Error.WriteLine($"==========================================================");
+                Console.Error.WriteLine($"Runner: {run.GetType().FullName}");
+                Console.Error.WriteLine($"==========================================================");
+                run.ExecuteTasks(30);
+                Console.Error.WriteLine();
+                Console.Error.WriteLine();
+            }
+        }
+
+
+        static IEnumerable<TaskRunnerBase> Runners
+        {
+            get
+            {
+                yield return new AndrewDemo.AndrewTaskRunner();
+                yield return new AndrewDemo.AndrewTaskRunner();
+            }
+        }
+    }
+}
