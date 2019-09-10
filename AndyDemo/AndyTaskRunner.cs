8aef5d4ecf620d5450a0e1e6f24c5e1f129b3769
DING-NB\Ding <andy19900208@gmail.com>
2019-09-10 20:21:27 +0800

    second version solution

---
+++ AndyDemo/AndyTaskRunner.cs
@@ -0,0 +1,35 @@
+using ParallelProcessPractice.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AndyDemo
+{
+    public class AndrewTaskRunner : TaskRunnerBase
+    {
+        public SemaphoreSlim sem_1 = new SemaphoreSlim(5);
+        public SemaphoreSlim sem_2 = new SemaphoreSlim(3);
+        public SemaphoreSlim sem_3 = new SemaphoreSlim(3);
+
+        public override void Run(IEnumerable<MyTask> tasks)
+        {
+            Parallel.ForEach(tasks, (myTask)=>{
+                sem_1.Wait();
+                myTask.DoStepN(1);
+                sem_1.Release();
+
+                sem_2.Wait();
+                myTask.DoStepN(2);
+                sem_2.Release();
+
+                sem_3.Wait();
+                myTask.DoStepN(3);
+                sem_3.Release();
+            });
+        }
+    }
+}
