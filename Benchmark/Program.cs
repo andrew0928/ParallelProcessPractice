46fe12ea52dc75c717178d44db51171673b2b864
Andrew Wu <chicken@chicken-house.net>
2019-09-23 01:07:03 +0800

    final

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -13,41 +13,74 @@ namespace Benchmark
                 Console.Error.WriteLine($"==========================================================");
                 Console.Error.WriteLine($"Runner: {run.GetType().FullName}");
                 Console.Error.WriteLine($"==========================================================");
+                GC.Collect();
                 run.ExecuteTasks(1000);
                 Console.Error.WriteLine();
                 Console.Error.WriteLine();
             }
-
-            //Console.WriteLine("Press [ENTER] to exit...");
-            //Console.ReadKey();
         }
 
         static IEnumerable<TaskRunnerBase> Runners
         {
             get
             {
-                yield return new NathanDemo.NathanTaskRunner();
-                yield return new LexDemo.LexTaskRunner();
-                yield return new SeanDemo.SeanRunner();
-                yield return new EPDemo.EPTaskRunner();
-                yield return new PhoenixDemo.PhoenixTaskRunner();
-                yield return new JulianDemo.TaskRunner();
-                yield return new GuluDemo.GuluTaskRunner();
-                yield return new JW.JWTaskRunnerV5();
-                yield return new AndyDemo.AndyTaskRunner();
-                yield return new MazeDemo.MazeTaskRunner();
+                // contributors from facebook
 
-                yield return new AndrewDemo.AndrewBasicTaskRunner1();
-                yield return new AndrewDemo.AndrewBasicTaskRunner2();
-                yield return new AndrewDemo.AndrewThreadTaskRunner1();
-                yield return new AndrewDemo.AndrewPipelineTaskRunner1();
-                yield return new AndrewDemo.AndrewPipelineTaskRunner2();
+                // multitask
+                yield return new LexDemo.LexTaskRunner();           // PR#1
 
+                // pipeline
+                yield return new EPDemo.EPTaskRunner();             // PR#3
+
+                // pipeline
+                yield return new SeanDemo.SeanRunner();             // PR#4
+
+                // pipeline
+                yield return new PhoenixDemo.PhoenixTaskRunner();   // PR#5
+
+                // pipeline
+                yield return new JulianDemo.TaskRunner();           // PR#6
+
+                // pipeline
+                yield return new GuluDemo.GuluTaskRunner();         // PR#7
+
+                // pipeline
+                yield return new JW.JWTaskRunnerV5();               // PR#8, PR#2
+
+                // multitask
+                yield return new AndyDemo.AndyTaskRunner();         // PR#10
+
+                // multitask
+                yield return new MazeDemo.MazeTaskRunner();         // PR#11, PR#9
+
+                // pipeline
+                yield return new NathanDemo.NathanTaskRunner();     // PR#12
+
+                // team members from 91APP
+
+                // pipeline
                 yield return new BorisDemo.BorisTaskRunner(5, 3, 3);
+
+                // pipeline
                 yield return new JolinDemo.JolinTaskRunner(3, 5, 3, 3);
+
+                // multitask
                 yield return new LeviDemo.LeviTaskRunner();
 
+                // my demo codes
 
+                // other
+                yield return new AndrewDemo.AndrewBasicTaskRunner1();
+                yield return new AndrewDemo.AndrewBasicTaskRunner2();
+
+                // multitask
+                yield return new AndrewDemo.AndrewThreadTaskRunner1();
+                yield return new AndrewDemo.AndrewThreadTaskRunner2();
+
+                // pipeline
+                yield return new AndrewDemo.AndrewPipelineTaskRunner1();
+                yield return new AndrewDemo.AndrewPipelineTaskRunner2();
+                yield return new AndrewDemo.AndrewPipelineTaskRunner3();
             }
         }
     }
--- ParallelProcessPractice.Core/TaskRunnerBase.cs
+++ ParallelProcessPractice.Core/TaskRunnerBase.cs
@@ -186,40 +186,46 @@ namespace ParallelProcessPractice.Core
                     do
                     {
                         stop = cancel.IsCancellationRequested;
-                        int[] x = null;
-                        lock (this._syncroot) x = this._thread_task_step_map.Keys.ToArray();
-                        foreach (int mtid in x)
-                        {
-                            if (idset.Contains(mtid) == false)
+
+                        //int[] x = null;
+                        lock (this._syncroot)
+                        {//x = this._thread_task_step_map.Keys.ToArray();
+
+                            foreach (int mtid in this._thread_last_step_map.Keys)
                             {
-                                idmap[idset.Count] = mtid;
-                                idset.Add(mtid);
+                                if (idset.Contains(mtid) == false)
+                                {
+                                    idmap[idset.Count] = mtid;
+                                    idset.Add(mtid);
+                                }
                             }
+
+
+                            output.Write($"{this._execution_timer.ElapsedMilliseconds},{this._allocated_memory_size},{this._dostep_wip_current[0]},{this._dostep_wip_current[1]},{this._dostep_wip_current[2]},{this._dostep_wip_current[3]}");
+                            output.Write($",{this._thread_last_step_map.Count}");
+                            output.Write($",{this._dostep_enter_counts[1]},{this._dostep_enter_counts[2]},{this._dostep_enter_counts[3]}");
+                            output.Write($",{this._dostep_exit_counts[1]},{this._dostep_exit_counts[2]},{this._dostep_exit_counts[3]}");
+
+
+                            for (int i = 0; i < idmap.Length; i++)
+                            {
+                                int mtid = idmap[i];
+                                if (mtid == 0 || this._thread_task_step_map.ContainsKey(mtid) == false || this._thread_task_step_map[mtid].seed == 0)
+                                {
+                                    output.Write(",");
+                                }
+                                else
+                                {
+                                    output.Write($",{this._thread_task_step_map[mtid].seed}#{this._thread_task_step_map[mtid].step}");
+                                }
+                            }
+                            output.WriteLine();
                         }
-
-
-                        output.Write($"{this._execution_timer.ElapsedMilliseconds},{this._allocated_memory_size},{this._dostep_wip_current[0]},{this._dostep_wip_current[1]},{this._dostep_wip_current[2]},{this._dostep_wip_current[3]}");
-                        output.Write($",{this._thread_task_step_map.Count}");
-                        output.Write($",{this._dostep_enter_counts[1]},{this._dostep_enter_counts[2]},{this._dostep_enter_counts[3]}");
-                        output.Write($",{this._dostep_exit_counts[1]},{this._dostep_exit_counts[2]},{this._dostep_exit_counts[3]}");
-
-
-                        for (int i = 0; i < idmap.Length; i++)
-                        {
-                            int mtid = idmap[i];
-                            if (mtid == 0 || this._thread_task_step_map.ContainsKey(mtid) == false || this._thread_task_step_map[mtid].seed == 0)
-                            {
-                                output.Write(",");
-                            }
-                            else
-                            {
-                                output.Write($",{this._thread_task_step_map[mtid].seed}#{this._thread_task_step_map[mtid].step}");
-                            }
-                        }
-                        output.WriteLine();
 
                         readyRunWait.Set();
-                        SpinWait.SpinUntil(() => { return this._execution_timer.ElapsedMilliseconds >= count * monitor_sample_time; });
+                        SpinWait.SpinUntil(() => {
+                            return this._execution_timer.ElapsedMilliseconds >= count * monitor_sample_time;
+                        });
 
                         output.Flush();
                         count++;
@@ -328,7 +334,8 @@ namespace ParallelProcessPractice.Core
             Console.Error.WriteLine($"  - Complete Step #2: {this._context._dostep_exit_counts[2]}");
             Console.Error.WriteLine($"  - Complete Step #3: {this._context._dostep_exit_counts[3]}");
 
-            string execute_log_filepath = @"execute-log.csv";
+            
+            string execute_log_filepath = $"execute-log-{DateTime.Now:yyyy-MMdd}-{taskCount}.csv";
             if (File.Exists(execute_log_filepath) == false)
             {
                 File.WriteAllText(execute_log_filepath, $"TEST_DT,RunnerName,WIP_ALL,WIP1,WIP2,WIP3,MEM_PEAK,CONTEXT_SWITCH_COUNT,TTFT,TTLT,AVG_WAIT\n");
